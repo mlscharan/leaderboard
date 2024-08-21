382465a8b046cc0c2a00cf34793210c74d691305
Mudili Lakshmi Srimannarayana Charan Teja <150829092+mlscharan@users.noreply.github.com>
2024-08-21 20:42:04 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,31 @@
+var tabs = document.querySelectorAll(".lboard_tabs ul li");
+var Previous = document.querySelector(".Previous");
+var Current = document.querySelector(".Current");
+var UpComing = document.querySelector(".UpComing");
+var UpComing = document.querySelectorAll(".lboard_item");
+
+tabs.forEach(function(tab){
+    tab.addEventListener("click",function(){
+        var currentdatali = tab.getAttribute("data-li");
+        
+        tabs.forEach(function(tab){
+            tab.classList.remove("active");
+        })
+
+        tab.classList.add("active");
+
+        items.forEach(function(item){
+            item.style.display="none";
+        })
+
+        if(currentdatali == "Previous"){
+            Previous.style.display = "block";
+        }
+        else if(currentdatali == "Current"){
+            Current.style.display = "block";
+        }
+        else{
+            UpComing.style.display = "block";
+        }
+    })
+})
